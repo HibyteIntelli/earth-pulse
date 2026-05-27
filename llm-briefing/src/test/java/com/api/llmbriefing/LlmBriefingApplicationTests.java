e5151472f3a545d75d87a30d9d353bc83f4a7e3b
Dragos <dragosinfinit10@gmail.com>
2026-05-27 15:44:01 +0300

    Init llm-briefing backend

---
+++ llm-briefing/src/test/java/com/api/llmbriefing/LlmBriefingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.llmbriefing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlmBriefingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
