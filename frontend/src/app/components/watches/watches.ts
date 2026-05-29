0b9acdca187225d515a18af0fba2fd11ccc00fba
DanutSpafiu <spafiu.spafiu@yahoo.com>
2026-05-29 10:23:47 +0300

    Added empty login, notifications, profile, and watches components

---
+++ frontend/src/app/components/watches/watches.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-watches',
+  imports: [],
+  templateUrl: './watches.html',
+  styleUrl: './watches.css',
+})
+export class Watches {}
