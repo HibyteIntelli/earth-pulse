b8cc6436e8838d2b0ebcdacb68538cf236c88104
Robert-Nicolas Trif <robert.trif@student.upt.ro>
2026-06-02 11:56:16 +0300

    Ingestion: replace @Data with @Getter/@Setter on NaturalEvent to avoid unstable hashCode

--- ingestion/src/main/java/ro/hibyte/ingestion/model/NaturalEvent.java
+++ ingestion/src/main/java/ro/hibyte/ingestion/model/NaturalEvent.java
@@ -1,14 +1,16 @@
 package ro.hibyte.ingestion.model;
 
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.OffsetDateTime;
 import java.util.HashSet;
 import java.util.Set;
 
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @Entity
 @Table(name = "natural_events")
