75399d9e9e4ef7ae1f68deb3612b3ae90d8b316c
Iasmina Gusca <iasmina.gusca23@gmail.com>
2026-06-04 15:19:13 +0300

    Add PostgreSQL Docker service and database connection tests

---
+++ notifier/src/test/java/ro/hibyte/notifier/DatabaseConnectionTest.java
@@ -0,0 +1,33 @@
+package ro.hibyte.notifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Testcontainers
+class DatabaseConnectionTest {
+
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:17");
+
+    @Autowired
+    DataSource dataSource;
+
+    @Test
+    void canConnectToDatabase() throws Exception {
+        try (Connection connection = dataSource.getConnection()) {
+            assertThat(connection.isValid(2)).isTrue();
+        }
+    }
+}
