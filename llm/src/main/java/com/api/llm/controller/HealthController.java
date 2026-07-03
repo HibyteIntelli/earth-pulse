0130ad192a1076d84d6142033e72155886f9c97b
Dragos <dragosinfinit10@gmail.com>
2026-07-03 17:43:38 +0300

    Remove unused Slf4j

--- llm/src/main/java/com/api/llm/controller/HealthController.java
+++ llm/src/main/java/com/api/llm/controller/HealthController.java
@@ -1,7 +1,6 @@
 package com.api.llm.controller;
 
 import com.api.llm.service.OllamaService;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +11,6 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
-@Slf4j
 public class HealthController {
 
     private final OllamaService ollamaService;
