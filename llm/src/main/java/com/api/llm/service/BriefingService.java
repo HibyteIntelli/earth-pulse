df8d090d30f2f78707e919cbc39dbec0f12d90f5
Petrutz14 <petru.craciunoiu@gmail.com>
2026-07-07 16:27:57 +0300

    -Removed double id check -Added GPU allocation and added flash in docker-compose.yaml for better speed

--- llm/src/main/java/com/api/llm/service/BriefingService.java
+++ llm/src/main/java/com/api/llm/service/BriefingService.java
@@ -96,13 +96,9 @@ public class BriefingService {
         }
         var id = new BriefingId(request.getEventId(), request.getReadingLevel());
 
-        if (isCached(request)) {
-            var briefing = briefingRepository.findById(id).orElseThrow();
-            return new BriefingResponseDto(briefing);
-        }
-
-        if (!ollamaService.checkStatus()) {
-            throw new LlmUnavailableException("Ollama is unreachable", null);
+        var briefingOpt = briefingRepository.findById(id);
+        if (briefingOpt.isPresent()) {
+            return new BriefingResponseDto(briefingOpt.get());
         }
 
         var llmRequest = new BriefingLLMRequestDto(request.getCategory(), request.getMagnitudeLevel(), request.getReadingLevel());
