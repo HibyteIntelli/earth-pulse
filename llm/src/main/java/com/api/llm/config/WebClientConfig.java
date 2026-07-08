f86aee4203e4f34d34da5537a58894c170559122
Petrutz14 <petru.craciunoiu@gmail.com>
2026-07-08 13:04:04 +0300

    -Added Olama timeout

--- llm/src/main/java/com/api/llm/config/WebClientConfig.java
+++ llm/src/main/java/com/api/llm/config/WebClientConfig.java
@@ -1,9 +1,12 @@
 package com.api.llm.config;
 
+import io.netty.channel.ChannelOption;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
 
 @Configuration
 public class WebClientConfig {
@@ -17,8 +20,12 @@ public class WebClientConfig {
             WebClient.Builder builder,
             @Value("${ollama.baseUrl}") String llmUrl) {
 
+        HttpClient httpClient = HttpClient.create()
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000);
+
         return builder
                 .baseUrl(llmUrl)
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
                 .build();
     }
 }
