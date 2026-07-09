b78147e25b6e44e3feea9faa970d14f54ff53e20
Robert-Nicolas Trif <robert.trif@student.upt.ro>
2026-07-09 20:22:59 +0300

    Ingestion: graceful shutdown for notifier async executor

--- ingestion/src/main/java/ro/hibyte/ingestion/config/AsyncConfig.java
+++ ingestion/src/main/java/ro/hibyte/ingestion/config/AsyncConfig.java
@@ -15,11 +15,14 @@ public class AsyncConfig {
     @Bean
     public Executor taskExecutor() {
         var executor = new ThreadPoolTaskExecutor();
+
         executor.setCorePoolSize(2);
         executor.setMaxPoolSize(5);
         executor.setQueueCapacity(100);
         executor.setThreadNamePrefix("notifier-async-");
         executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        executor.setAwaitTerminationSeconds(30);
         executor.initialize();
 
         return executor;
