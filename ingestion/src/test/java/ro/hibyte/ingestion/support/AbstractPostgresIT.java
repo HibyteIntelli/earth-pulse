92b8be6b439dfaf0ff967f6d9992de453481e349
Robert-Nicolas Trif <robert.trif@student.upt.ro>
2026-07-17 22:49:14 +0300

    Add shared Postgres testcontainer for DB slice tests

---
+++ ingestion/src/test/java/ro/hibyte/ingestion/support/AbstractPostgresIT.java
@@ -0,0 +1,15 @@
+package ro.hibyte.ingestion.support;
+
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.postgresql.PostgreSQLContainer;
+
+@ActiveProfiles("test")
+public abstract class AbstractPostgresIT {
+    @ServiceConnection
+    static PostgreSQLContainer postgres = new PostgreSQLContainer("postgres:17");
+
+    static {
+        postgres.start();
+    }
+}
